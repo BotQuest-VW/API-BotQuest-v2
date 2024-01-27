ffa003b0a4ab1937ce5eed0dd00f6417baa9d794
juliaathar <juathar@gmail.com>
2024-01-27 17:43:30 -0300

    :sparkles: Mudança de repositório e correção de erros
    Feito git checkout pra versão anterior por conta de erros com o upload de imagem, feito adição de JsonBackReference para consertar erro de serialização e desserialização com relacionamento bidirecional das entidades de Usuário e Chamado. Fazendo mudança de repositório

---
+++ src/main/java/com/botquest/BotQuestAPI/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package com.botquest.BotQuestAPI.controllers;
+
+import com.botquest.BotQuestAPI.dtos.LoginDto;
+import com.botquest.BotQuestAPI.dtos.TokenDto;
+import com.botquest.BotQuestAPI.models.UsuarioModel;
+import com.botquest.BotQuestAPI.services.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class LoginController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Valid LoginDto dados) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.gerarToken((UsuarioModel) auth.getPrincipal());
+
+        return ResponseEntity.status(HttpStatus.OK).body(new TokenDto(token));
+    }
+}
+
---
+++ src/main/java/com/botquest/BotQuestAPI/services/AuthService.java
@@ -0,0 +1,25 @@
+package com.botquest.BotQuestAPI.services;
+
+import com.botquest.BotQuestAPI.repositories.UsuarioRepository; // Importa a classe UsuarioRepository para acessar os dados do usuário
+import org.springframework.beans.factory.annotation.Autowired; // Importa a anotação @Autowired para injeção de dependências
+import org.springframework.security.core.userdetails.UserDetails; // Importa a interface UserDetails do Spring Security
+import org.springframework.security.core.userdetails.UserDetailsService; // Importa a interface UserDetailsService do Spring Security
+import org.springframework.security.core.userdetails.UsernameNotFoundException; // Importa a exceção UsernameNotFoundException do Spring Security
+import org.springframework.stereotype.Service; // Importa a anotação @Service para marcar a classe como um serviço
+
+public class AuthService implements UserDetailsService { // Declaração da classe AuthService que implementa UserDetailsService
+    @Autowired // Injeta o UsuarioRepository para acessar os dados dos usuários
+    UsuarioRepository usuarioRepository;
+
+    @Override // Sobrescreve o método da interface UserDetailsService
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var usuario = usuarioRepository.findByEmail(username); // Busca o usuário pelo email
+
+        if (usuario == null){
+            throw new UsernameNotFoundException("Usuário não encontrado"); // Lança uma exceção se o usuário não for encontrado
+        }
+
+        // Retorna um objeto UserDetails (neste caso, retorna nulo, mas normalmente você retornaria os detalhes do usuário)
+        return null;
+    }
+}
